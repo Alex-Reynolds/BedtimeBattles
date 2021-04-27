2926289ae30bf4c2db0de573be5fcdbe74a2aefd
endiodobasic <endio@iastate.edu>
2021-04-27 01:19:33 -0500

    health when colliding with enemies

--- BedtimeBattlesGame/Assets/Player.cs
+++ BedtimeBattlesGame/Assets/Player.cs
@@ -14,14 +14,20 @@ public class Player : MonoBehaviour {
   }
 
   // Update is called once per frame
-  void Update() {
-    if (Input.GetKeyDown(KeyCode.Space)) {
-      TakeDamage(20);
-    }
-  }
+  //void Update() {
+    //if (Input.GetKeyDown(KeyCode.Space)) {
+    //  TakeDamage(20);
+    //}
+  //}
 
   void TakeDamage(int damage) {
     currentHealth -= damage;
     healthBar.SetHealth(currentHealth);
   }
+  void OnCollisionEnter2D(Collision2D col) {
+    if (col.gameObject.tag == "Enemy") {
+      TakeDamage(20);
+    }
+  }
+
 }
