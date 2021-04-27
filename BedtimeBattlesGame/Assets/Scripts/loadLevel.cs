0b0ca95d649f59df3feb5d1a41d1cef6d49a4039
Amdahl <djamdahl@iastate.edu>
2021-04-27 13:17:33 -0500

    A Lot - 21 Savage

---
+++ BedtimeBattlesGame/Assets/Scripts/loadLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadLevel : MonoBehaviour
+{
+    public int levelToLoad;
+
+    void onTriggerEnter2D(Collider2D collision)
+    {
+      GameObject collisionGameObject = collision.gameObject;
+
+      if(collisionGameObject.name == "Player")
+      {
+        SceneManager.LoadScene(levelToLoad);
+      }
+    }
+}
