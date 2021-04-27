52662b6032034f978ea86f6758a4b68c0b123673
Amdahl <djamdahl@iastate.edu>
2021-04-27 12:20:55 -0500

    levelDoor now appears after five second in Level One (uses DelayedAppearance.cs)

---
+++ BedtimeBattlesGame/Assets/DelayedAppear.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedAppear : MonoBehaviour
+{
+
+  void Awake()
+  {
+    hideDoor();
+  }
+    // Start is called before the first frame update
+    void Start()
+    {
+      StartCoroutine(doorAppear());
+    }
+
+    public IEnumerator doorAppear()
+    {
+      yield return new WaitForSeconds(5f);
+      gameObject.GetComponent<Renderer>().enabled = true;
+    }
+
+    public void hideDoor()
+    {
+      gameObject.GetComponent<Renderer>().enabled = false;
+    }
+
+
+}
