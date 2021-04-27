88100a41964d3034ed40c411fc5e7fb188303414
Amdahl <djamdahl@iastate.edu>
2021-04-26 22:30:10 -0500

    Added Level Two, Added Zombie Sprite, Added Code to Load Levels
    However, the Load Level function is currently not functioning.

---
+++ BedtimeBattlesGame/Assets/loadLevel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadLevel : MonoBehaviour
+{
+    public int iLevelToLoad;
+    public string sLevelToLoad;
+    public bool useIntegerToLoadLevel = false;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void onTriggerEnter2D(Collider2D collision)
+    {
+      GameObject collisionGameObject = collision.gameObject;
+
+      if(collisionGameObject.name == "Player")
+      {
+        LoadScene();
+      }
+    }
+
+    void LoadScene()
+    {
+      if(useIntegerToLoadLevel)
+      {
+        SceneManager.LoadScene(iLevelToLoad);
+      }
+      else
+      {
+        SceneManager.LoadScene(sLevelToLoad);
+      }
+    }
+}
